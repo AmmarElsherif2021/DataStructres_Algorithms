58749d701799c871908ba8e389110073eb3b2209
AmmarElsherif2021 <ammar.alshareef2014@gmail.com>
2023-04-03 21:55:44 +0200

    jjjj

--- buildTree2.py
+++ buildTree2.py
@@ -18,37 +18,46 @@ class Node:
         self.val=val
         self.left=left
         self.right=right
-
+        
     def getVal(self):
         return self.val
     def getNode(self):
-        return [self.val,self.right,self.left,]
+        return [self.val,self.right,self.left]
 
 class Solution:
+    tree=[]
     def createTree(self,preorder,inorder):
+        global tree
         tempNode=list()
         while inorder:
             index=inorder.index(preorder[0])
             del preorder[0]
             root=Node(inorder[index])
             root.left=self.createTree(preorder,inorder[:index])
-        
-            root.right=self.createTree(preorder,inorder[index+1:])
             
-            tempNode=root.getNode()
-            if tempNode[1]:
-                tempNode[1]=tempNode[1].getVal()
-            if tempNode[2]:
-                tempNode[2]=tempNode[2].getVal()
-            print(tempNode)
+            root.right=self.createTree(preorder,inorder[index+1:])
+           
+            tempNotion=root.getNode()
+            if tempNotion[1]:
+                tempNotion[1]=tempNotion[1].getVal()
+            if tempNotion[2]:
+                tempNotion[2]=tempNotion[2].getVal()
+            #print(tempNotion)
+            self.tree.append(tempNotion)
+            
             return root
                 
                
     def buildTree(self,preorder,inorder):
         root=self.createTree(preorder,inorder)
+        treeList=list()
+        treeList.append(self.tree[-1])
+        r=self.tree
+        
+        return r
         
         
         
-        pass
+        
 solution=Solution()
-print(solution.createTree([3,9,20,15,5,7,1],[9,3,15,5,20,7,1]))
+print(solution.buildTree([3,9,20,15,5,7,1],[9,3,15,5,20,7,1]))
